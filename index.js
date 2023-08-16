38578f304f02c48b9c1b2173729bd67f97ba7746
Pmangeot <pierre.mangeot@gmail.com>
2023-08-16 14:06:28 +0200

    sligth correction

--- index.js
+++ index.js
@@ -3,22 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Reglage caching headers pour images statiques
 const staticOptions = {
-  maxAge: '7d', // Cache images de 7 jours
+  maxAge: '7d',
 };
 app.use('/static', express.static('public', staticOptions));
 
-// reglages ejs dossiers public router
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.use(express.static('public'));
 const router = require('./app/routers/router');
 
-// lancement du router
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 app.use(router);
 
-// listener
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
